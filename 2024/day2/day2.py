d32e6748fa654870bf77cf6e8d7a9ea9129e0984
George Pramantiotis <giorgospram@gmail.com>
2024-12-02 18:44:28 +0200

    2024_day2

--- 2024/day2/day2.py
+++ 2024/day2/day2.py
@@ -2,53 +2,37 @@ def is_valid(numbers: list):
     is_increasing = all(0 < (b - a) <= 3 for a, b in zip(numbers, numbers[1:]))
     is_decreasing = all(-3 <= (b - a) < 0 for a, b in zip(numbers, numbers[1:]))
 
-    if is_increasing or is_decreasing:
-        return 1
-    else:
-        return 0
-
+    return is_increasing or is_decreasing
 
 def problem_1():
     count = 0
-
     with open ('day2_input.txt', 'r') as file:
         for line in file:
-
             numbers = list(map(int, line.strip().split()))
-
             if not numbers:
                 continue
-     
             if is_valid(numbers):
                 count += 1
-
     return count
 
 
 
 def problem_2():
     count = 0
-
     with open ('day2_input.txt', 'r') as file:
         for line in file:
-
             numbers = list(map(int, line.strip().split()))
-
-            if not numbers:
+            if not numbers or len(numbers) <= 1:
                 continue
-
             if is_valid(numbers):
                 count += 1
                 continue
-
             else:
                 for i in range(len(numbers)):
                     numbers_new = numbers[:i] + numbers[i+1:]
-
                     if is_valid(numbers_new):
                         count += 1
                         break
-
     return count
 
 
